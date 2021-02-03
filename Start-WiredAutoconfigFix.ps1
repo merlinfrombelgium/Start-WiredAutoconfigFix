37525d28e93c8b7fa5944f179695162c026a0163
merlinfrombelgium <merlinfrombelgium@gmail.com>
2021-02-03 10:18:09 +0100

    Now uses media type 802.3 to identify net adapters

--- Start-WiredAutoconfigFix.ps1
+++ Start-WiredAutoconfigFix.ps1
@@ -21,23 +21,23 @@ $FirstPart = $TemplateContent | Select-Object -First $($SearchStringRow-1)
 $LastPart = $TemplateContent | Select-Object -Index ($($SearchStringRow-1)..$TotalRows)
 
 # Define the customization part to insert into $SetupCompleteTemplate
-# If your network connections are not named "Ethernet", change the Get-NetAdapter filter in the content below
 $CustomPart = (
 "echo --- Running 802.1x Fixes ---",
 "echo %DATE%-%TIME% `"Custom Configuration`" >> %WINDIR%\setupcomplete.log",
-"echo %DATE%-%TIME% `"Disabling NetAdapter for 5 seconds`" >> %windir%\setupcomplete.log",
-"echo Disabling NetAdapter for 5 seconds",
-"powershell.exe -command `"Get-NetAdapter *eth* | Disable-NetAdapter -Confirm:`$false -PassThru -Verbose`" >> %WINDIR%\setupcomplete.log",
+"echo %DATE%-%TIME% `"Disabling NetAdapters for 5 seconds`" >> %windir%\setupcomplete.log",
+"echo Disabling NetAdapters for 5 seconds",
+"powershell.exe -command `"$NetAdapters = Get-NetAdapter -Physical | where {$_.PhysicalMediaType -eq 802.3}`"",
+"powershell.exe -command `"Disable-NetAdapter $NetAdapters -Confirm:`$false -PassThru -Verbose`" >> %WINDIR%\setupcomplete.log",
 "powershell.exe -command `"Start-Sleep 5`"",
 
-"echo %DATE%-%TIME% `"Enabling NetAdapter and sleeping for 15 seconds`" >> %windir%\setupcomplete.log",
-"echo Enabling NetAdapter and waits 15 seconds",
-"powershell.exe -command `"Get-NetAdapter *eth* | Enable-NetAdapter -Confirm:`$false -PassThru -Verbose`" >> %WINDIR%\setupcomplete.log",
+"echo %DATE%-%TIME% `"Enabling NetAdapters and sleeping for 15 seconds`" >> %windir%\setupcomplete.log",
+"echo Enabling NetAdapters and waits 15 seconds",
+"powershell.exe -command `"Enable-NetAdapter $NetAdapters -Confirm:`$false -PassThru -Verbose`" >> %WINDIR%\setupcomplete.log",
 "powershell.exe -command `"Start-Sleep 15`"",
 
 "echo %DATE%-%TIME% `"Restarting Wired Autoconfig Services`" >> %WINDIR%\setupcomplete.log",
 "echo Restarting dot3svc and waits 15 seconds",
-"powershell.exe -command `"restart-service dot3svc -PassThru -verbose`" >> %WINDIR%\setupcomplete.log",
+"powershell.exe -command `"Restart-Service dot3svc -PassThru -verbose`" >> %WINDIR%\setupcomplete.log",
 "powershell.exe -command `"Start-Sleep 15`"",
 
 "ipconfig >> %WINDIR%\setupcomplete.log",
